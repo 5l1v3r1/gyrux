7eb06f230070a2a684ce8a3e5299714bbc356865
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-01-27 00:43:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,27 @@
+#!/bin/sh
 
+read -p $'What is your OS? (linux/darwin/freebsd/openbsd/netbsd): ' OS
+if [[ $OS == "darwin" || $OS == "freebsd" || $OS == "openbsd" || $OS == "netbsd" ]]
+then
+    if [[ $OS == "darwin" ]]
+    then
+        {
+        cp bin/darwin-amd64/gyrux /usr/local/bin
+        chmod +x /usr/local/bin/gyrux
+        } &> /dev/null
+        exit
+    else
+        {
+        cp bin/$OS-amd64/gyrux /bin
+        chmod +x /bin/gyrux
+        } &> /dev/null
+        exit
+    fi
+else
+    read -p $'What is your architecture? (amd64/arm64/i386): ' $ARCH
+    {
+    cp bin/linux-$ARCH /bin
+    chmod +x /bin/gyrux
+    } &> /dev/null
+    exit
+fi
